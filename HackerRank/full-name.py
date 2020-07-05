25dfba16dcc93bd540097532c5a673c5f956c6e0
Ian <kamanuian@gmail.com>
2020-07-05 13:47:45 +0300

    full-name

---
+++ HackerRank/full-name.py
@@ -0,0 +1,8 @@
+def print_full_name(a, b):
+    a = str(input())
+    b = str(input())
+    print("Hello " + a + " " + b + "! You just delved into python.")
+
+a = ""
+b =""
+print(print_full_name(a, b))
